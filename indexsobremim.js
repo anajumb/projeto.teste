1cff574e52ba05b328fd87fee589c8fa11ce191e
ana julia macedo barreto <110177266+anajumb@users.noreply.github.com>
2023-05-22 17:41:18 -0300

    Add files via upload

---
+++ indexsobremim.js
@@ -0,0 +1,16 @@
+// var a = document.getElementById("area")
+
+// a.addEventListener("mouseover", entrar)
+// a.addEventListener("mouseout", sair)
+
+//     function entrar(){
+//         a.innerText="ANA julia macedo barreto ana julia macedo barreto"
+//     }
+//     // function sair(){
+        
+    // }
+let numero = 10
+
+console.log(numero)
+console.log(numero)
+console.log(numero-numero)
